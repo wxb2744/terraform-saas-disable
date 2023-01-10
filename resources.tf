3045a8da2a1593babd3a9d7d2c34cc49168382fc
Warren Barnes <warrenb@netapp.com>
2023-01-10 11:26:58 +0000

    Created variables for security

--- resources.tf
+++ resources.tf
@@ -2,18 +2,17 @@
 
 resource "netapp-cloudmanager_cvo_azure" "cvo-azure" {
   provider = netapp-cloudmanager
-  name = "TerraformCVOAzure001"
+  name = "6140a726-2a7c-40dd-ac42-34f9d8cc5d97"
   location = "northeurope"
   availability_zone = 1
-  subscription_id = "941adedb-c7f9-4c2e-b98f-eaadb3cd35e4"
-  subnet_id = "subnet1"
-  vnet_id = "ubs"
-  vnet_resource_group = "ubs-terraform-test"
+  subscription_id = var.subscription_id
+  subnet_id = var.subnet_id
+  vnet_id = var.vnet_id
+  vnet_resource_group = var.vnet_resource_group
   data_encryption_type = "AZURE"
   storage_type = "Premium_LRS"
-  svm_password = "Netapp123!"
-  client_id = "i7LaZ5E4gydOpjIml4GHoC06HwEA6nYRclients"
-  #workspace_id = "i7LaZ5E4gydOpjIml4GHoC06HwEA6nYRclients"
+  svm_password = var.svm_password
+  client_id = var.client_id
   capacity_tier = "Blob"
   writing_speed_state = "NORMAL"
   capacity_package_name = "Freemium"
--- variables.tf
+++ variables.tf
@@ -6,3 +6,31 @@ variable "cloudmanager_sa_secret_key" {
 variable "cloudmanager_sa_client_id" {
   type = string
 }
+
+variable "connector_host" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "svm_password" {
+  type = string
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "subnet_id" {
+  type = string
+}
+
+variable "vnet_id" {
+  type = string
+}
+
+variable "vnet_resource_group" {
+  type = string
+}
