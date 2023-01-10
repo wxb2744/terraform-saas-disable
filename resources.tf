5978e6f8f1ce0da95c128314517f35ae0e50e5ea
Warren Barnes <warrenb@netapp.com>
2023-01-10 11:35:48 +0000

    updated resources.tf

--- resources.tf
+++ resources.tf
@@ -5,7 +5,6 @@ resource "netapp-cloudmanager_cvo_azure" "cvo-azure" {
   name = "6140a726-2a7c-40dd-ac42-34f9d8cc5d97"
   location = "northeurope"
   availability_zone = 1
-<<<<<<< HEAD
   subscription_id = var.subscription_id
   subnet_id = var.subnet_id
   vnet_id = var.vnet_id
@@ -14,16 +13,6 @@ resource "netapp-cloudmanager_cvo_azure" "cvo-azure" {
   storage_type = "Premium_LRS"
   svm_password = var.svm_password
   client_id = var.client_id
-=======
-  subscription_id = ""
-  subnet_id = "subnet1"
-  vnet_id = "vnet"
-  vnet_resource_group = "vnet_resource_group"
-  data_encryption_type = "AZURE"
-  storage_type = "Premium_LRS"
-  svm_password = ""
-  client_id = "" 
->>>>>>> ac40939333bc541f9795a65f5b16258ecd641746
   capacity_tier = "Blob"
   writing_speed_state = "NORMAL"
   capacity_package_name = "Freemium"
